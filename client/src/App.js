9f8a590d241ca9863c5650b2f24a3564e3a6a67a
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2022-08-03 01:24:12 -0300

    Calling all the games

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import './App.scss';
 import ButtonHome from './components/ButtonHome/ButtonHome';
+import Home from './components/Home/Home';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [games, setGames] = useState([]);
+
+  const callGames = async () => {
+    console.log('test')
+    const games = await axios.get('http://localhost:3001/videogames');
+    console.log(games.data)
+    setGames(games.data);
+  }
   return (
     <div className="App">
       <h1>PrtenGames</h1>
-      <ButtonHome props={callGames}/>
+      <ButtonHome callGames={callGames}/>
+      {games.length > 0 && <Home />}
     </div>
   );
 }
